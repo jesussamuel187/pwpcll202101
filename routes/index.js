ef662c66c74f397b4e40c2c19d82dd6ba211f2a5
Jesussamuel <L171130095@gamadero.tecnm.mx>
2021-04-15 20:06:25 -0500

    agregando nombre de compañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', autor: 'jesus samuel',jesus: 'jesus'});
+  res.render('index', { title: 'Express', autor: 'jesus samuel',jesus: 'jesus',  compañia: 'Awsome software'});
 });
 
 /* codigo nuevo. */
