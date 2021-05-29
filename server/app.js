75da7fe4e7472a49b5572662a12f9d5d999b188e
Jesussamuel <L171130095@gamadero.tecnm.mx>
2021-05-28 23:33:01 -0500

    sesion27

--- server/app.js
+++ server/app.js
@@ -1,63 +1,75 @@
- import  createError from 'http-errors';
- import  express from 'express';
- import  path  from 'path';
- import  cookieParser from   'cookie-parser';
- import  logger from 'morgan';
- 
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
 import indexRouter from '@s-routes/index';
 import usersRouter from '@s-routes/users';
-// webpack modules 
+
+// Webpack Modules
+
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMidddleware from 'webpack-hot-middleware';
-import webpackConfig, { plugins } from '../webpack.dev.config';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackDevConfig from '../webpack.dev.config';
-//consultar el modo en que se este ejecutando la aplicacion.
-const env = process.env.NODE_ENV || 'developement';
-//se crea la aplicacion express
-var app = express();
-//verificando el modo de ejecucion de la aplicacion
-if(env === 'development'){
-  console.log('> Excecuting int Development Mode : Webpack Hot Reloading');
-  //paso 1 agregando la ruta del HMR
-  //reload = true: habilita la recarga del front end cuando hay cambios en el codigo fuente del front end
-  //timeout = mil: tiempo de espera entre recarga y recarga de la paguina
-  webpackConfig.entry = ['webpack-hot-middleware/client?reload=true&timeout=mil',webpackConfig.entry];
 
-//paso 2 agregamos el plugin 
-webpackConfig,plugins.push(new webpack.HotModuleReplacementPlugin());
-//paso 3 crear el compilador de webpack
-const compiler = webpack(webpackConfig);
-//paso 4 agregando el middleware ala cadena de middlewares de nuestra aplicacion
-app.use(webpackDevMiddleware(compiler,{
-  publicPath: webpackDevConfig.output.publicPath
-}));
-//paso 5 agregando el webpack hot middleware
-app.use(webpackHotMidddleware(compiler));
-} else{
-  console.log('> Excecuting int Production Mode...');   
+// Consultar el modo en que se esta ejecutando el programa
+const env = process.env.NODE_ENV || 'development';
+
+// se crea la aplicacion express
+const app = express();
+
+// verificando el modo de ejecucion de la aplicacion
+if (env === "development") {
+  // eslint-disable-next-line no-console
+  console.log('> Excecuting in Development Mode: Webpack Hot Reloading');
+  // Agrega la ruta del HMR
+  // reload=true: Habilita la recarga del frontend cuando hay cambios en el código
+  // 'fuente del fronten
+  // timeout=1000: Tiempo de espera entre recarga y recarga de la pagina.
+  webpackDevConfig.entry = [
+    "webpack-hot-middleware/client?reload=true&timeout=1000",
+    webpackDevConfig.entry,
+  ];
+  // Agregamos el plugin
+  webpackDevConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // crer el compilador de webpack
+  const compiler = webpack(webpackDevConfig);
+  // agregamos el middleware a la cadena de middlewares
+  // de nuestra aplicacion
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: webpackDevConfig.output.publicPath,
+    })
+  );
+
+  // agrega el webpack hot middleware
+  app.use(webpackHotMiddleware(compiler));
+} else {
+  // eslint-disable-next-line no-console
+  console.log('> Excecuting in Production Mode......');
 }
-
 // view engine setup       //(hbs= halderbals)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));    // es para que nos muestren las peticiones que hacen.
-app.use(express.json());  // comvierte el http a formato json.(es un conversor )
-app.use(express.urlencoded({ extended: false }));   //para todas las peticiones de url.
-app.use(cookieParser());   //manejo de cookies
-app.use(express.static(path.join(__dirname,'..', 'public')));  //este es uno de los mas importates
+app.use(logger('dev')); // es para que nos muestren las peticiones que hacen.
+app.use(express.json()); // comvierte el http a formato json.(es un conversor )
+app.use(express.urlencoded({ extended: false })); // para todas las peticiones de url.
+app.use(cookieParser()); // manejo de cookies
+app.use(express.static(path.join(__dirname, '..', 'public'))); // este es uno de los mas importates
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
